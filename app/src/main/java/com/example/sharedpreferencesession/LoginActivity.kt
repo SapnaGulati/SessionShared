69c320b20fef91661dcb07ddde8c1466f9b176e9
Sapna Gulati <69303444+SapnaGulati@users.noreply.github.com>
2021-04-01 12:37:43 +0530

    Update LoginActivity.kt

--- app/src/main/java/com/example/sharedpreferencesession/LoginActivity.kt
+++ app/src/main/java/com/example/sharedpreferencesession/LoginActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -13,24 +12,19 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import org.json.JSONObject
 import java.io.File
-
 
 class LoginActivity : AppCompatActivity() {
     private val SHARED_PREFS = "shared_prefs"
     private val NAME_KEY = "name_key"
     private val PASSWORD_KEY = "password_key"
-    private val mapKey = "map1"
     private var map: HashMap<String, String> = hashMapOf()
     lateinit var sName: String
     lateinit var sPass: String
     lateinit var sharedPreferences: SharedPreferences
-    private lateinit var username: String
-    private lateinit var password: String
     private var mapper = ObjectMapper()
+    lateinit var name: TextView
+    lateinit var pass: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,11 +32,10 @@ class LoginActivity : AppCompatActivity() {
 
         val signup = findViewById<TextView>(R.id.signup)
         val logbtn = findViewById<Button>(R.id.logbtn)
-        val name = findViewById<View>(R.id.user_name) as EditText
-        val pass = findViewById<View>(R.id.password) as EditText
-        username = name.text.toString().trim()
-        password = pass.text.toString().trim()
+        name = findViewById<View>(R.id.user_name) as EditText
+        pass = findViewById<View>(R.id.password) as EditText
         sharedPreferences = this.getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
         sName = sharedPreferences.getString(NAME_KEY, null).toString()
         sPass = sharedPreferences.getString(PASSWORD_KEY, null).toString()
 
@@ -53,6 +46,8 @@ class LoginActivity : AppCompatActivity() {
         }
 
         logbtn.setOnClickListener {
+            val username = name.text.toString().trim()
+            val password = pass.text.toString().trim()
             if (username != null && password != null) {
                 try {
                     map = mapper.readValue(
@@ -61,54 +56,23 @@ class LoginActivity : AppCompatActivity() {
                 } catch (e: Exception) {
                     e.printStackTrace()
                 }
+
                 if (map.keys.toString().contains(username) && map.values.toString().contains(password)) {
-//                    val l: MutableSet<String> = map.keys
-//                    if(username in l) {
-//                    val value = map.getValue(pass.toString())
-//                        Toast.makeText(this, "${map[username]}  ${map.values}", Toast.LENGTH_SHORT).show()
-//                    if (map.keys.toString()) {
+                    if (map[username] == password) {
+                        editor.putString("isLoggedIn", "true")
+                        editor.apply()
                         Toast.makeText(this, "Logged-in Successfully", Toast.LENGTH_SHORT).show()
                         val i = Intent(this, HomeActivity::class.java)
                         startActivity(i)
                         finish()
-//                    } else {
-//                        Toast.makeText(this, "Either user-name or password is incorrect. No such account exists with this user-name and password", Toast.LENGTH_SHORT).show()
-//                    }
-//                    }
+                    }
+                    else {
+                        Toast.makeText(this, "Either user-name or password is incorrect. No such account exists with this user-name and password", Toast.LENGTH_SHORT).show()
+                    }
                 } else {
-                    Toast.makeText(this, "else key : $sName $sPass", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Not such account exists... Please create your account then try to log-in.", Toast.LENGTH_SHORT).show()
                 }
             }
         }
-    }
-
-    private fun loadMap() {
-        val l = intent
-        val outputMap = l.getSerializableExtra("map")
-        if (sharedPreferences != null) {
-            val jsonString = sharedPreferences.getString(mapKey, JSONObject().toString())
-            val jsonObject = JSONObject(jsonString)
-            val keysItr = jsonObject.keys()
-            getHashMap(key = outputMap as HashMap<String, String>?)
-            while (keysItr.hasNext()) {
-                val key = keysItr.next()
-                outputMap?.set(key, key)
-            }
-        }
-    }
-
-    private fun getHashMap(key: HashMap<String, String>?): Map<String, String> {
-        val prefs = PreferenceManager.getDefaultSharedPreferences(applicationContext)
-        val gson = Gson()
-//        val json = prefs.getString(key.toString(), "")
-        val json = gson.toJson(map)
-        val type = object : TypeToken<HashMap<String?, String?>?>() {}.type
-        //        val emptyMap = gson.toJson(map) as HashMap<String, String>
-//        return gson.fromJson(
-//                sharedPreferences.getString(key, emptyList),
-//                object : TypeToken<ArrayList<ProductData>>() {
-//                }.type
-//        )
-        return gson.fromJson<HashMap<String, String>?>(json, type)
     }
 }
