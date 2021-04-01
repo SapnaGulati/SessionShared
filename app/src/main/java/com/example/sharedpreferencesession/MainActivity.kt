6162628bb6022563fd1745e97d53e1e7ad84e21f
Sapna Gulati <69303444+SapnaGulati@users.noreply.github.com>
2021-04-01 12:39:54 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/sharedpreferencesession/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreferencesession/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.example.sharedpreferencesession
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var sharedPreferences: SharedPreferences
+    lateinit var editor: SharedPreferences.Editor
+    private val SHARED_PREFS = "shared_prefs"
+    lateinit var isLogin: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val login = findViewById<TextView>(R.id.login)
         val signup = findViewById<TextView>(R.id.signup)
+        sharedPreferences = this.getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE)
+        editor = sharedPreferences.edit()
 
         login.setOnClickListener {
             val i = Intent(this, LoginActivity::class.java)
@@ -21,6 +30,16 @@ class MainActivity : AppCompatActivity() {
 
         signup.setOnClickListener {
             val i = Intent(this, SignupActivity::class.java)
+            startActivity(i)
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        isLogin = sharedPreferences.getString("isLoggedIn", null).toString()
+        if(isLogin == "true") {
+            val i = Intent(this, HomeActivity::class.java)
             startActivity(i)
             finish()
         }
