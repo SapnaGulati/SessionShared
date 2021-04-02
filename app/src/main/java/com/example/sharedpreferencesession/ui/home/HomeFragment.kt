d979944463c6cedb5ffa79b29bdd6d599efad5e6
Sapna Gulati <69303444+SapnaGulati@users.noreply.github.com>
2021-04-02 11:18:32 +0530

    Update HomeFragment.kt

--- app/src/main/java/com/example/sharedpreferencesession/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/sharedpreferencesession/ui/home/HomeFragment.kt
@@ -15,11 +15,10 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
-    private lateinit var pb: ProgressBar
-    private lateinit var rv: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,29 +27,27 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        pb = root.findViewById(R.id.progressBar)
-        rv = root.findViewById(R.id.recyclerView)
-        return root
-    }
-    private val request = ServiceBuilder.buildService(TmdbEndPoints::class.java)
-    private val call = request.getMovies(getString(R.string.api_key))
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        call.enqueue(object:  Callback<PopularMovies> {
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val pb = root?.findViewById<ProgressBar>(R.id.progressBar)
+        val rv = root?.findViewById<RecyclerView>(R.id.recyclerView)
+        val request = ServiceBuilder.buildService(TmdbEndPoints::class.java)
+        val call = request.getMovies("dd05a3971bda331641d65bfd7776ccb0")
+        call.enqueue(object : Callback<PopularMovies> {
             override fun onResponse(call: Call<PopularMovies>, response: Response<PopularMovies>) {
+                if (pb != null) {
                     pb.visibility = View.GONE
-                    rv.apply {
-                        setHasFixedSize(true)
-                        layoutManager = LinearLayoutManager(activity)
-                        adapter = MoviesAdapter(response.body()!!.results)
-                    }
                 }
+                rv?.apply {
+                    setHasFixedSize(true)
+                    layoutManager = LinearLayoutManager(activity)
+                    adapter = MoviesAdapter(response.body()!!.results)
+                }
+            }
 
             override fun onFailure(call: Call<PopularMovies>, t: Throwable) {
                 Toast.makeText(activity, "Failed to load data", Toast.LENGTH_SHORT).show()
             }
         })
+        return root
     }
 }
