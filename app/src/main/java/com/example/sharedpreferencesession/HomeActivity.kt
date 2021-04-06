d0a8384ada1fa4d163a5330c24a00b9d2d2017a7
Sapna Gulati <69303444+SapnaGulati@users.noreply.github.com>
2021-04-06 11:34:49 +0530

    Update HomeActivity.kt

--- app/src/main/java/com/example/sharedpreferencesession/HomeActivity.kt
+++ app/src/main/java/com/example/sharedpreferencesession/HomeActivity.kt
@@ -4,38 +4,36 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
-import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import com.example.sharedpreferencesession.ui.dashboard.DashboardFragment
-import com.example.sharedpreferencesession.ui.home.HomeFragment
-import com.example.sharedpreferencesession.ui.notifications.NotificationsFragment
+import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class HomeActivity : AppCompatActivity() {
+
+class HomeActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     private val SHARED_PREFS = "shared_prefs"
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var editor: SharedPreferences.Editor
     private lateinit var navHostFragment: NavHostFragment
-    private lateinit var navController: NavController
+    private lateinit var navControl: NavController
     private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val fragment1 = HomeFragment()
-        val fragment2 = DashboardFragment()
-        val fragment3 = NotificationsFragment()
+        val navView = findViewById<BottomNavigationView>(R.id.nav_view)
 
         navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.findNavController()
+        navControl = navHostFragment.navController
         sharedPreferences = this.getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE)
         editor = sharedPreferences.edit()
         toolbar = findViewById(R.id.toolbar)
@@ -44,7 +42,12 @@ class HomeActivity : AppCompatActivity() {
                 setOf(
                         R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
                 )
-        )
+                )
+
+        NavigationUI.setupWithNavController(navView, navControl)
+        navView.setOnNavigationItemSelectedListener(this)
+
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
@@ -63,43 +66,32 @@ class HomeActivity : AppCompatActivity() {
                 false
             }
         }
-
-        val navView = findViewById<BottomNavigationView>(R.id.nav_view)
-
-        navView.setOnNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.navigation_home -> {
-                    supportFragmentManager.beginTransaction().remove(fragment3).commit()
-                    if (savedInstanceState == null) {
-                        replaceFragment(fragment1)
-                        supportActionBar?.title = "Home Fragment"
-                    }
-                    true
-                }
-                R.id.navigation_dashboard -> {
-                    supportFragmentManager.beginTransaction().remove(fragment1).commit()
-                    replaceFragment(fragment2)
-                    supportActionBar?.title = "DashBoard Fragment"
-                    true
-                }
-                R.id.navigation_notifications -> {
-                    supportFragmentManager.beginTransaction().remove(fragment2).commit()
-                    replaceFragment(fragment3)
-                    supportActionBar?.title = "Notification Fragment"
-                    true
-                }
-                else -> false
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.navigation_home -> {
+                val navOptions = NavOptions.Builder().setPopUpTo(R.id.mobile_navigation, true).build()
+                Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.navigation_home)
+                true
             }
+            R.id.navigation_dashboard -> {
+                if (isValidDestination(R.id.navigation_dashboard)) {
+                    Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.navigation_dashboard)
+                }
+                true
+            }
+            R.id.navigation_notifications -> {
+                if (isValidDestination(R.id.navigation_notifications)) {
+                    Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.navigation_notifications)
+                }
+                supportActionBar?.title = "Notification Fragment"
+                true
+            }
+            else -> false
         }
     }
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().apply {
-            if (fragment != null) {
-                supportFragmentManager.popBackStack()
-            }
-            add(R.id.container, fragment)
-            addToBackStack(null)
-            commit()
-        }
+
+    private fun isValidDestination(destination: Int): Boolean {
+        return destination != Navigation.findNavController(this, R.id.nav_host_fragment).currentDestination!!.id
     }
 }
